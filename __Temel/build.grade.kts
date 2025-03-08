f4a17ecd48f65828eea1a047395f9a8c3d98432c
MDuymaz <81718348+MDuymaz@users.noreply.github.com>
2025-03-09 01:00:07 +0300

    Update build.grade.kts

--- __Temel/build.grade.kts
+++ __Temel/build.grade.kts
@@ -1,1 +1,18 @@
+version = 0
 
+cloudstream {
+    authors     = listOf("mduymaz")
+    language    = "tr"
+    description = "Deneme"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Movie")
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.setfilmizle.de&sz=%size%"
+}
